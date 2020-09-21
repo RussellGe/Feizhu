95b3d7ea8817dc208f65e91cbcf96e573e2f639e
RussellGe <russellxin0601@gmail.com>
2020-09-21 19:49:29 +0800

    init2

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <router-link to="/list">
+        <div>Home</div>
+    </router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
